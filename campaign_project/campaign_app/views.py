2852cd756d2195875303c9761e351e4de9a52c86
Koriyou <53308239+Koriyou@users.noreply.github.com>
2024-01-17 07:13:54 +0100

    made the iterrative session thing work

--- campaign_project/campaign_app/views.py
+++ campaign_project/campaign_app/views.py
@@ -1,10 +1,40 @@
 # Campaign_Manager/campaign_app/views.py
-from django.shortcuts import render
+
 
 def index(request):
     return render(request, 'campaign_app/index.html')
 
+import os
+import markdown
+from django.conf import settings
+from django.shortcuts import render
+
+
 def campaign1(request):
-    return render(request, 'campaign_app/Campaign1.html')
+    # Construct the path to the campaigns_markdown directory
+    markdown_directory = os.path.join(settings.BASE_DIR, 'campaigns_markdown')
+
+    # Initialize a list to store session content
+    session_content = []
+
+    # Iterate through session files in the campaign directory
+    for filename in os.listdir(markdown_directory):
+        if filename.endswith(".md"):
+            # Read the Markdown content from the file
+            with open(os.path.join(markdown_directory, filename), "r") as file:
+                markdown_content = file.read()
+
+                # Convert Markdown to HTML
+                html_content = markdown.markdown(markdown_content)
+
+                # Add the HTML content to the list
+                session_content.append(html_content)
+
+    # Pass the markdown content to the template
+    context = {"markdown_content": session_content}
+    return render(request, "campaign_app/Campaign1.html", context)
+
+
+
 
 # Define more views as needed
---
+++ campaign_project/campaigns_markdown/session1.md
@@ -0,0 +1,18 @@
+**Session 1 Notes - Clean**_Date: 26/12/2023_
+
+- The session commenced with [[Teddy]] bidding farewell to the village of Bearne. Before his departure, he received several parting gifts from the villagers:
+	- [[Grigor]]
+	- [[Alaric]]
+	- [[Isolde]]
+	- [[Lyra]]
+- [[Teddy]]’s journey is taking him to the port town of Verena, which is expected to be a 10.5-day journey.
+	- Presently, he finds himself four days into his voyage when an extraordinary event occurred:
+		- On the third day of his journey, an unusual flower descended from the sky. This peculiar flower has the remarkable ability to grow when nourished and is composed of an otherworldly metal. Astonishingly, the flower fused itself with [[Teddy]]’s hand, turning it into a golden appendage.
+		- This enigmatic flower, which goes by the name Christa, possesses knowledge related to alchemy and transmutation. It has made a promise to educate [[Teddy]] in these arcane arts if he assists her in achieving a transformation of her own.
+
+
+the session has seen the arrival of 2 new players 
+
++1 character = [[Solomon]]
+
++1 character = [[Jack]]
