4fdefd2442bb68301fcea3cbb99adecad60fcbde
Koriyou <53308239+Koriyou@users.noreply.github.com>
2024-01-17 06:24:43 +0100

    Managed to make the campaign1 html work

--- campaign_project/campaign_app/urls.py
+++ campaign_project/campaign_app/urls.py
@@ -1,9 +1,8 @@
-# campaign_app/urls.py
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('Campaign1.html', views.campaign1, name='campaign1'),
     # Add more URL patterns as needed
 ]
--- campaign_project/campaign_app/views.py
+++ campaign_project/campaign_app/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'campaign_app/index.html')
 
+def campaign1(request):
+    return render(request, 'campaign_app/Campaign1.html')
+
 # Define more views as needed
